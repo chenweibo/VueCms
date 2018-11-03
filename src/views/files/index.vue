cb6af882af3e9f696fff8752637166b99a43d911
chenweibo <563960993@qq.com>
2018-11-03 11:28:05 +0800

    file

--- src/views/files/index.vue
+++ src/views/files/index.vue
@@ -1,8 +1,57 @@
 <template>
   <div class="app-container">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item >当前路径</el-breadcrumb-item>
+      <el-breadcrumb-item>/</el-breadcrumb-item>
+      <el-breadcrumb-item>view</el-breadcrumb-item>
+      <el-breadcrumb-item>home</el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="line"/>
     <div class="filter-container">
       <el-button v-waves class="filter-item" type="primary" size="mini" >上传</el-button>
-
     </div>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="文件名"
+        width="180"/>
+      <el-table-column
+        prop="name"
+        label="大小"
+        width="180"/>
+      <el-table-column
+        prop="address"
+        label="修改时间"/>
+      <el-table-column
+        prop="cc"
+        label="操作"/>
+    </el-table>
   </div>
 </template>
+<script>
+export default {
+  name: 'File',
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄',
+        cc: '11'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.line{
+   width: 100%;
+   border-top: 1px solid #f0f0f0;
+   margin: 20px 0 10px 0;
+}
+</style>
+
